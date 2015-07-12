dff28c7ee5c0736c28de92a6f54720774ee3a177
Claudio Rodriguez <claudio.rdgz@gmail.com>
2015-07-12 15:06:21 -0400

    removed counter 8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1038,7 +1038,7 @@ var sfApp={
         var remainTime = Math.ceil(totalTime - (totalTime * progress));
         var notifyStr='';
         var allMightyHeight = parseFloat(document.body.clientHeight);
-        if($(window).scrollTop() < (allMightyHeight - allMightyHeight * 0.5)){
+        if($(window).scrollTop() < (allMightyHeight - (allMightyHeight * 0.18))){
             if($(window).width()>979){
                 $shareBox.fadeOut(100);
             }
