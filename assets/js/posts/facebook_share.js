79da38448531005cbe9e53068608784db685db2a
Claudio Rodriguez <claudio.rdgz@gmail.com>
2015-07-24 18:39:58 -0400

    new post stuff

--- assets/js/posts/facebook_share.js
+++ assets/js/posts/facebook_share.js
@@ -13,3 +13,45 @@ window.fbAsyncInit = function() {
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+function getStyle(x, styleProp) {
+    if (x.currentStyle) var y = x.currentStyle[styleProp];
+    else if (window.getComputedStyle) var y = document.defaultView.getComputedStyle(x, null).getPropertyValue(styleProp);
+    return y;
+}
+
+function feedDialog(id, caption, picture) {
+    var title, description;
+    var div = document.getElementById(id);
+    var pic;
+    var cap = caption ? caption : '';
+    for (var i = 0, childNode; i <= div.childNodes.length; i ++) {
+        childNode = div.childNodes[i];
+        if(childNode) {
+            if (/mdl-card__supporting-text/.test(childNode.className)) {
+                description = childNode.textContent;
+            } else if(/mdl-card__title/.test(childNode.className)) {
+                title = childNode.innerText;
+                pic = !picture ? getStyle(childNode, 'background-image') : picture;
+            }
+        }
+    }
+    FB.ui(
+        {
+            method: 'feed',
+            name: title,
+            link: window.location.href,
+            description: description,
+            caption: cap,
+            picture: pic,
+            display: 'popup'
+        },
+        function(response, show_error) {
+            if (response && response.post_id) {
+                console.log('Post was published.');
+            } else {
+                console.log('Post was not published.');
+            }
+        }
+    );
+}
