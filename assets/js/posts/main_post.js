558ae447be83e6746d7514c7484d09f63e1183e1
Claudio Rodriguez <claudio.rdgz@gmail.com>
2015-07-25 19:03:32 -0400

    playing with ads

--- assets/js/posts/main_post.js
+++ assets/js/posts/main_post.js
@@ -29,6 +29,31 @@ function slugify(text)
         .replace(/-+$/, '');            // Trim - from end of text
 }
 
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+    script.async = true;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var adsLoaded = function() {
+    [].forEach.call(document.querySelectorAll('.adsbygoogle'), function(){
+        var i = 0;
+    });
+};
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
     var pageContent = document.getElementsByClassName('post-content')[0];
     var navigationLinks  = document.getElementsByClassName('mdl-navigation')[0];
@@ -69,4 +94,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
             drawer.className = className;
         }
     });
+    loadScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', adsLoaded);
 });
